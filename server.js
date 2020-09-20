b33168fbb93b163737f8279ba4e25d960ab0f18f
usmonalixon <usmonalixonnizomov@gmail.com>
2020-09-20 11:05:44 +0500

    heroku fix deploy

--- server.js
+++ server.js
@@ -7,92 +7,92 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/build"));
-app.get("/", (req,res) => res.sendFile(__dirname + "/build/index.html"))
-
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 mongoose.connect(
-    process.env.MONGODB_URL || 
-    "mongodb://localhost/react-shopping-cart-db", {
+  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 const Product = mongoose.model(
-    "products",
-    new mongoose.Schema({
-        _id: { type: String, default: shortid.generate },
-        title: String,
-        description: String,
-        image: String,
-        price: Number,
-        availableSizes: [String],
-    })
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
 );
 
 app.get("/api/products", async (req, res) => {
-    const products = await Product.find({});
-    res.send(products);
+  const products = await Product.find({});
+  res.send(products);
 });
 
 app.post("/api/products", async (req, res) => {
-    const newProduct = new Product(req.body);
-    const savedProduct = await newProduct.save();
-    res.send(savedProduct);
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
 });
 
 app.delete("/api/products/:id", async (req, res) => {
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-    res.send(deletedProduct)
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
 });
 
-
 const Order = mongoose.model(
-    "order",
-    new mongoose.Schema(
+  "order",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      email: String,
+      name: String,
+      address: String,
+      total: Number,
+      cartItems: [
         {
-            _id: {
-                type: String,
-                default: shortid.generate
-            },
-            email: String,
-            name: String,
-            address: String,
-            total: Number,
-            cartItems: [
-                {
-                    _id: String,
-                    title: String,
-                    price: Number,
-                    count: Number,
-                },
-            ],
+          _id: String,
+          title: String,
+          price: Number,
+          count: Number,
         },
-        {
-            timestamps: true,
-        }
-    )
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
 );
 
 app.post("/api/orders", async (req, res) => {
-    if (!req.body.name ||
-        !req.body.email ||
-        !req.body.address ||
-        !req.body.total ||
-        !req.body.cartItems
-    ) {
-        return res.setDefaultEncoding({ message: 'data is required' });
-    }
-    const order = await Order(req.body).save();
-    res.send(order);
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total ||
+    !req.body.cartItems
+  ) {
+    return res.send({ message: "Data is required." });
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
 });
 app.get("/api/orders", async (req, res) => {
-    const orders = await Order.find({});
-    res. send(orders)
+  const orders = await Order.find({});
+  res.send(orders);
 });
 app.delete("/api/orders/:id", async (req, res) => {
-    const order = await Order.findByIdAndDelete(req.params.id);
-    res.send(order)
+  const order = await Order.findByIdAndDelete(req.params.id);
+  res.send(order);
 });
 
 const port = process.env.PORT || 5000;
